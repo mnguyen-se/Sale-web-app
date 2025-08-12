a678ffb6f7217829c02674ee6de699eade7f2cd6
mnguyen-se <minhnguyen08112004@gmail.com>
2025-08-12 09:27:57 +0700

    update project

---
+++ Downloads/Web-sale-app/Web-sale-app/src/main/java/com/example/Web_sale_app/WebSaleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Web_sale_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSaleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSaleAppApplication.class, args);
+	}
+
+}
